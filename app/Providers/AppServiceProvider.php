52a0b15cf9b07dc991393d4b7e2a0741f05299c4
wuqi <wuqi226@gmail.com>
2019-10-27 23:12:36 +0800

    add sql log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // add debug query log
+        if (env('APP_DEBUG')) {
+            DB::listen(function($query) {
+                $sql = '';
+                $splits = explode('?', $query->sql);
+                $swith = count($splits) - 1;
+                foreach ($splits as $cnt => $split) {
+                    $sql .= $cnt < $swith ? ($split . "'".  $query->bindings[$cnt]. "'") : $split;
+                }
+                File::append(
+                    storage_path('logs/sql-'. date('Y-m-d') .'.log'),
+                    sprintf('date: %s | execution time: %d | sql: %s ;', date('Y-m-d H:i:s'), $query->time, $sql) . PHP_EOL
+                );
+            });
+        }
     }
 }
